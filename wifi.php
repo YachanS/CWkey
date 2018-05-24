843c96055c6363e9891c5190aea0cfaa4d34d6e0
yachan <settama.yachanvv@gmail.com>
2018-05-24 17:22:22 +0200

    update php 2

--- wifi.php
+++ wifi.php
@@ -1,4 +1,33 @@
-<?php include('include/head.php'); ?>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sufee Admin - HTML5 Admin Template</title>
+    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-icon.png">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/flag-icon.min.css">
+    <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
+    <!-- <link rel="stylesheet" href="assets/css/bootstrap-select.less"> -->
+    <link rel="stylesheet" href="assets/scss/style.css">
+    <link rel="stylesheet" href="assets/css/lib/chosen/chosen.min.css">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+
+</head>
 
 <body>
 
@@ -20,10 +49,7 @@
             </div>
         </div>
 
-    <form action="" method="post" class="">
-        <div class="content mt-3">
-            <div class="animated fadeIn">
-                <div class="row">
+        <form>
         <div class="col-xs-6 col-sm-6">
                         <div class="card">
                             <div class="card-header">
@@ -44,7 +70,7 @@
                                 <strong class="card-title">Select wifi</strong>
                             </div>
                             <div class="card-body">
-                                  <select data-placeholder="Choose a user" class="standardSelect" tabindex="1">
+                                  <select data-placeholder="Choose a wifi" class="standardSelect" tabindex="1">
                                     <option value=""></option>
                                     <option value="United States">OpenSpace</option>
                                     <option value="United Kingdom">Reunion</option>
@@ -57,11 +83,26 @@
                         <button type="submit" class="btn btn-success btn-sm" style="position: relative; right:0%;">Create</button>
                     </center>
                 </div>
-                </div>
-            </div>
-        </div>
     </form>
+
+
+
+    <script src="assets/js/vendor/jquery-2.1.4.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/lib/chosen/chosen.jquery.min.js"></script>
+
+    <script>
+        jQuery(document).ready(function() {
+            jQuery(".standardSelect").chosen({
+                disable_search_threshold: 10,
+                no_results_text: "Oops, nothing found!",
+                width: "100%"
+            });
+        });
+    </script>
 
 </body>
 
-<?php include('include/footer.php'); ?>
+</html>
