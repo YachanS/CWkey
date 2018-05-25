da916797cb21a19355dbf9b927755ded8de0a803
yachan <settama.yachanvv@gmail.com>
2018-05-25 11:21:14 +0200

    update wifi.php

--- wifi.php
+++ wifi.php
@@ -1,33 +1,4 @@
-<!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Sufee Admin - HTML5 Admin Template</title>
-    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="apple-touch-icon" href="apple-icon.png">
-    <link rel="shortcut icon" href="favicon.ico">
-
-    <link rel="stylesheet" href="assets/css/normalize.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/themify-icons.css">
-    <link rel="stylesheet" href="assets/css/flag-icon.min.css">
-    <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
-    <!-- <link rel="stylesheet" href="assets/css/bootstrap-select.less"> -->
-    <link rel="stylesheet" href="assets/scss/style.css">
-    <link rel="stylesheet" href="assets/css/lib/chosen/chosen.min.css">
-
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
-
-    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
-
-</head>
+<?php include('include/head.php'); ?>
 
 <body>
 
@@ -49,60 +20,61 @@
             </div>
         </div>
 
-        <form>
-        <div class="col-xs-6 col-sm-6">
-                        <div class="card">
-                            <div class="card-header">
-                                <strong class="card-title">Select user</strong>
-                            </div>
-                            <div class="card-body">
-                                  <select data-placeholder="Choose a user" class="standardSelect" tabindex="1">
-                                    <option value=""></option>
-                                    <option value="United States">Aurelien</option>
-                                    <option value="United Kingdom">Mathieu</option>
-                                </select>
-                            </div>
-                        </div>
-        </div>
-        <div class="col-xs-6 col-sm-6">
-                        <div class="card">
-                            <div class="card-header">
-                                <strong class="card-title">Select wifi</strong>
-                            </div>
-                            <div class="card-body">
-                                  <select data-placeholder="Choose a wifi" class="standardSelect" tabindex="1">
-                                    <option value=""></option>
-                                    <option value="United States">OpenSpace</option>
-                                    <option value="United Kingdom">Reunion</option>
-                                </select>
-                            </div>
-                        </div>
-        </div>
-                <div class="form-actions form-group">
-                    <center>
-                        <button type="submit" class="btn btn-success btn-sm" style="position: relative; right:0%;">Create</button>
-                    </center>
+<form>
+    <div class="col-xs-6 col-sm-6">
+        <div class="card">
+            <div class="card-header">
+                <strong class="card-title">User</strong>
+            </div>
+                <div class="card-body">
+                    <input list="users" id="" name="" class="col-lg-12"/>
+                        <datalist id="users">
+                            <option value="">
+                            <option value="Mathieu">
+                            <option value="Aurélien">
+                            <option value="Axel">
+                            <option value="Yachan">
+                        </datalist>
                 </div>
-    </form>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-6">
+        <div class="card">
+            <div class="card-header">
+                <strong class="card-title">wifi</strong>
+            </div>
+            <div class="card-body">
+                <input list="wifi" id="" name="" class="col-lg-12"/>
+                    <datalist id="wifi">
+                        <option value="Openspace">
+                        <option value="Reunion">
+                    </datalist>
+            </div>
+        </div>
+    </div>
+                           
+    <div class="form-actions form-group">
+        <center>
+            <button type="submit" class="btn btn-success btn-sm" style="position: relative; right:;">Create</button>
+        </center>
+    </div>
+</form>
 
-
-
-    <script src="assets/js/vendor/jquery-2.1.4.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/plugins.js"></script>
-    <script src="assets/js/main.js"></script>
-    <script src="assets/js/lib/chosen/chosen.jquery.min.js"></script>
-
-    <script>
-        jQuery(document).ready(function() {
-            jQuery(".standardSelect").chosen({
-                disable_search_threshold: 10,
-                no_results_text: "Oops, nothing found!",
-                width: "100%"
-            });
-        });
-    </script>
+<div class="col-xs-12 col-sm-12">
+    <div class="card-header">
+        <div>
+            <strong class="card-title">Contents</strong>
+        </div>
+        <div class="card-body">
+            <div class="alert alert-success" role="alert">
+                <h4 class="alert-heading">Well done!</h4>
+                <p>Le mot de passe pour l'utilisateur "" pour le wifi "" est :</p><hr>
+                <p class="mb-0">mot de passe</p>
+            </div>
+        </div>
+    </div>
+</div>
 
 </body>
 
-</html>
+<?php include('include/footer.php'); ?>
